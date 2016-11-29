ac8cacdadc6f115062b0ee8739562f6ef1c13e77
andralutu <andra@simula.no>
2016-11-29 04:03:15 +0100

    updates

--- generalized_ecn/files/experiment.sh
+++ generalized_ecn/files/experiment.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-/usr/bin/multispider -p ECNASpider1 -I /opt/monroe/input_test.csv -o /tmp/ECNASpider1.txt
 mkdir /monroe/
 mkdir /monroe/results
-mv /tmp/output.txt /monroe/results/
-
+/usr/bin/multispider -p ECNSpider1 -I /opt/monroe/input_test.csv -o /tmp/ECNASpider1.txt
+mv /tmp/* /monroe/results/
